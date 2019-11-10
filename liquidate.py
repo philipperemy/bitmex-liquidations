efcb75e834fb80c97dc5d8937af90ad4a570ceca
Philippe Remy <premy@cogent.co.jp>
2019-11-10 16:32:10 +0900

    nicer interface

--- liquidate.py
+++ liquidate.py
@@ -1,29 +1,85 @@
 import json
-import threading
-import sys
-import websocket
 import logging
+import sys
+import threading
+from datetime import datetime
+from time import sleep
+
+import websocket
 
 logger = logging.getLogger(__name__)
+
+
+class LiquidationPrinter:
+    # Just to have a colorful console.
+    class BColor:
+        HEADER = '\033[95m'
+        OKBLUE = '\033[94m'
+        OKGREEN = '\033[92m'
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
+
+    HEADER = ['Symbol', 'B/S', 'USD Value', 'Quantity', 'Price', 'Time']
+
+    @staticmethod
+    def print_header():
+        LiquidationPrinter.print_to_console('Symbol', 'B/S', 'USD Value', 'Quantity', 'Price', 'Time')
+
+    @staticmethod
+    def print_to_console(symbol, bs, usd_value, qty, price, time):
+        if bs == 'Buy':
+            is_long = LiquidationPrinter.BColor.OKGREEN
+        elif bs == 'Sell':
+            is_long = LiquidationPrinter.BColor.FAIL
+        else:
+            is_long = ''
+        line = is_long
+        line += symbol.ljust(10)
+        line += bs.ljust(6)
+        try:
+            usd_value = format(int(usd_value), ",d")
+        except ValueError:
+            pass
+        line += usd_value.ljust(12)
+        try:
+            qty = format(int(qty), ",d")
+        except ValueError:
+            pass
+        line += qty.ljust(10)
+        line += '@'.ljust(4)
+        line += str(price).ljust(10)
+        try:
+            time = time.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+        except AttributeError:
+            pass
+        line += time
+        line += LiquidationPrinter.BColor.ENDC
+        logger.info(line)
+
 
 class BitMEXLiquidation:
 
     def __init__(self, url='wss://www.bitmex.com/realtime?subscribe=liquidation', symbol=None):
         if symbol is not None:
             url += ':' + symbol
-        logger.info("Connecting to %s" % url)
+        logger.info("Connecting to %s." % url)
         self._connect(url)
         self.num_pongs = 0
         logger.info('Connected to WS.')
+        sleep(5)  # just wait for bitmex to answer Welcome to the BitMEX Realtime API!
+        LiquidationPrinter.print_header()
 
     def exit(self):
-        '''Call this to exit - will close websocket.'''
+        """Call this to exit - will close websocket."""
         logger.info('exit()')
         self.exited = True
         self.ws.close()
 
     def _connect(self, url):
-        '''Connect to the websocket in a thread.'''
+        """Connect to the websocket in a thread."""
         self.ws = websocket.WebSocketApp(url,
                                          on_message=self._on_message,
                                          on_close=self._on_close,
@@ -38,29 +94,48 @@ class BitMEXLiquidation:
         self.wst.start()
 
     def _on_pong(self, ws, msg):
-        if self.num_pongs % 100 == 0:
-            logger.info('Waiting for liquidation... Might take a while... Check https://app.rek.to/')
+        # if self.num_pongs % 100 == 0:
+        # logger.info('Waiting for liquidation... Might take a while... Check https://app.rek.to/')
         self.num_pongs += 1
 
     def _on_message(self, ws, message):
-        '''Handler for parsing WS messages.'''
+        """Handler for parsing WS messages."""
         message = json.loads(message)
-        logger.info(json.dumps(message, indent=2, sort_keys=True))
+
+        # print(message)
+        if 'action' in message and message['action'] == 'insert':
+            assert len(message['data']) == 1
+            liquidation = message['data'][0]
+            qty = float(liquidation['leavesQty'])
+            price = float(liquidation['price'])
+            side = liquidation['side']
+            symbol = liquidation['symbol']
+            # Symbol	B/S	USD Value	Quantity		Price	Time
+            # ETHUSD	Buy	4,598	2,723	@	189	15:01
+            usd_value = price * qty if symbol != 'XBTUSD' else qty
+            LiquidationPrinter.print_to_console(symbol, side, usd_value, qty, price, datetime.now())
+        elif 'info' in message:
+            logger.info('[<] ' + message['info'])
+        elif 'subscribe' in message:
+            logger.info('[<] Subscribed to %s' % message['subscribe'] + '.')
 
     def _on_error(self, ws, error):
-        '''Called on fatal websocket errors. We exit on these.'''
+        """Called on fatal websocket errors. We exit on these."""
         logger.info('on_error()', error)
 
     def _on_open(self, ws):
-        '''Called when the WS opens.'''
+        """Called when the WS opens."""
         logger.info("Websocket Opened.")
 
     def _on_close(self, ws):
-        '''Called on websocket close.'''
+        """Called on websocket close."""
         logger.info('Websocket Closed')
 
 
 if __name__ == '__main__':
+    # LiquidationPrinter.print_header()
+    # LiquidationPrinter.print_to_console('XBTUSD', 'Buy', 10000, 10000, 8848, datetime.now())
+    # LiquidationPrinter.print_to_console('XBTUSD', 'Sell', 10000, 10000, 8848, datetime.now())
     logging.basicConfig(format='%(asctime)12s - %(message)s', level=logging.INFO)
     _symbol = sys.argv[1] if len(sys.argv) > 1 else None
     ws = BitMEXLiquidation(symbol=_symbol)
