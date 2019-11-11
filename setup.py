c0728a9a3582fbf3ac413589b974c2f6ef5e8244
Philippe Remy <premy@cogent.co.jp>
2019-11-11 21:05:28 +0900

    1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bitmex-liquidation',
-    version='1.0',
+    version='1.0.1',
     description='BitMEX Liquidation Python Wrapper',
     author='Philippe Remy',
     license='MIT',
